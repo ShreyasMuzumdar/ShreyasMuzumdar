0fa77f69bce26e564a822acf58f6f46c0ba350c1
Guru Muzumdar <guru@atomcommerce.io>
2024-10-09 23:00:47 -0400

    updated all pages to be responsive

--- scripts.js
+++ scripts.js
@@ -1,3 +1,30 @@
+// Google Tag Manager
+(function(w,d,s,l,i){
+    w[l]=w[l]||[];
+    w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+    var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),
+        dl=l!='dataLayer'?'&l='+l:'';
+    j.async=true;
+    j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+    f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MX52ZK34');
+// End Google Tag Manager
+
+// Google Tag Manager (noscript)
+document.addEventListener('DOMContentLoaded', function() {
+    var noscript = document.createElement('noscript');
+    var iframe = document.createElement('iframe');
+    iframe.src = "https://www.googletagmanager.com/ns.html?id=GTM-MX52ZK34";
+    iframe.height = "0";
+    iframe.width = "0";
+    iframe.style.display = "none";
+    iframe.style.visibility = "hidden";
+    noscript.appendChild(iframe);
+    document.body.insertBefore(noscript, document.body.firstChild);
+});
+// End Google Tag Manager (noscript)
+
 document.addEventListener('DOMContentLoaded', function() {
     // Typing effect
     const textElement = document.getElementById('typing-effect');
@@ -43,26 +70,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Set initial mode
     document.body.classList.add('light-mode');
+
 });
 
-// Scroll Indicator
-window.onscroll = function() {
-    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    const scrolled = (winScroll / height) * 100;
-    document.querySelector('.scroll-indicator').style.width = scrolled + "%";
-};
 
-// Fade-in on scroll
-const sections = document.querySelectorAll('section');
-
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-    });
-});
 
 sections.forEach(section => {
     observer.observe(section);
